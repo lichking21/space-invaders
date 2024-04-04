900f0cdea05b76ab46cf9a9d87186083b98083c0
[A]man <87190510+lichking21@users.noreply.github.com>
2024-04-04 12:58:09 +0200

    Add files via upload

---
+++ Bullet.h
@@ -0,0 +1,23 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+class Bullet {
+
+public:
+
+	sf::RectangleShape bullet;
+	sf::Clock clock;
+
+	bool isFired = false;
+	float bulletSpeed = 0.5f;
+
+	void shoot(sf::RenderWindow& win, const sf::Vector2f& playerPos);
+};
+
+
+#endif // !BULLET_H
